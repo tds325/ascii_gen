950b9571b471c3255b65232678827d91702886b7
tds325 <tds325@nau.edu>
2023-10-16 16:56:54 -0700

    Image segmentation and processing

--- ascii.py
+++ ascii.py
@@ -1,13 +1,18 @@
 import numpy as np
 import os
-from math import floor
+from math import floor, ceil
 from PIL import Image
+from textwrap import dedent
 
 MAX_OUTPUT_HEIGHT = 50
 MAX_OUTPUT_WIDTH = 160
 SMALL_DEFAULT = 10
 MED_DEFAULT = 20
 LRG_DEFAULT = 36
+XLRG_DEFAULT = 75
+COLOR_MAX=255
+TRANSFORM=1
+DELIMITER=0
 
 # compensate for characters being more tall than wide
 H_MULTIPLIER = 3
@@ -20,25 +25,55 @@ def main():
 
     output_to_ascii(imglist[imgnum])
 
-def determine_image_size(img, size = "large"):
-    height_dict = { "small" : SMALL_DEFAULT,
-                    "medium" : MED_DEFAULT, 
-                    "large" : LRG_DEFAULT }
+def decide_classification(sub_image, image, dict_len, 
+                          data_transform, delimiter) -> int:
+    num = data_transform(sub_image, image)
+    ascii_class = delimiter(num, dict_len)
+    return ascii_class
 
-    width, height = img.size
+def determine_image_size(image, size:str = "xlarge"):
+    height_dict = { "small"  : SMALL_DEFAULT,
+                    "medium" : MED_DEFAULT, 
+                    "large"  : LRG_DEFAULT,
+                    "xlarge" : XLRG_DEFAULT }
+
+    width, height = image.size
     aspect_ratio = width / height
     
     if width > MAX_OUTPUT_HEIGHT or height > MAX_OUTPUT_WIDTH:
         height = height_dict[size]
         width = int(height * aspect_ratio * H_MULTIPLIER)
-        
+
+    aspect_ratio = width / height
+
+    if(not validate_size(image.size[0], width)):
+        width = (image.size[0] - 1) // 2
+    if(not validate_size(image.size[1], height)):
+        height = (image.size[1] - 1) // 2
+
+    if(width / height < aspect_ratio):
+        print("adjusting height to fit aspect ratio...")
+        while((width / height < aspect_ratio) and aspect_ratio - width/height > 0.5):
+            height -= 1
+    else:
+        print("adjusting width to fit aspect ratio...")
+        while(width / height > aspect_ratio and width/height - aspect_ratio > 0.5):
+            width -= 1
+
     return (width,height)
 
 def determine_segment_values(image, segment_list, dict_len):
     segment_key_list = []
     for segment in segment_list:
         sub_image = image[segment[2]:segment[3], segment[0]:segment[1]]
-        segment_key_list.append(sub_image_to_key(sub_image, dict_len))
+        if 0 in sub_image.shape:
+            print(dedent(f'''\
+            segment: {segment}
+            sub_image: {sub_image}
+            ERROR: segment of image with at least one axis of dimension 0.
+            Check image segmentation logic - exiting program.'''))
+            quit()
+        segment_key_list.append(sub_image_to_key(sub_image, dict_len, image))
     return segment_key_list
 
 def input_images(files):
@@ -51,6 +86,49 @@ def input_images(files):
         except:
             print(f'unable to open file {img_name}')
     return imglist
+
+def n_exclusive_segments(axis_size:int, num_segments:int)->list:
+    seg_list = []
+
+    if (num_segments) > (axis_size + 1)/2:
+        print(dedent(f'''\
+            Number of segments too large for axis with too few pixels.  
+            ({axis_size}, {num_segments}) Exiting program.'''))
+        quit()
+
+    pixel_width:int = 1
+    temp:int = 1
+    while((temp*num_segments)+(num_segments - 1) <= axis_size):
+        pixel_width = temp
+        temp += 1
+
+    # calculate 'leftovers' 
+    # *remainder will always be less than the number of segments
+    # otherwise the pixel_width could have simply been incremented by one
+    leftover = axis_size - ((pixel_width * num_segments) + (num_segments - 1))
+    #print(f"pixel_width: {pixel_width}, ({axis_size}, {num_segments}), r {leftover}")
+
+    # evenly distribute the remainder throughout the segments
+    #partition_quotient = num_segments / leftover
+    remainder_list = []
+    middle_index = (num_segments // 2)
+    remainder_list = [middle_index for i in range(leftover)]
+
+    # precipitate from center by adding/subtracting from segment index
+    # subtract the distance from the center of the array
+    for i in range(leftover):
+        remainder_list[i] = remainder_list[i] + (i - leftover // 2)
+
+    previous = -1
+    for i in range(num_segments):
+        endsegment_index:int = previous + pixel_width + 1
+        if i in remainder_list:
+            endsegment_index += 1
+        temple:tuple = (previous+1, endsegment_index)
+        seg_list.append(temple)
+        previous = endsegment_index
+        
+    return seg_list
 
 def output_to_ascii(image):
     ascii_dict = { 0: '%',
@@ -77,38 +155,50 @@ def output_to_ascii(image):
             print()
         index += 1
 
-def print_imglist(imglist):
+def print_imglist(imglist:list):
     for num, img in enumerate(imglist):
         print(f'{num}:\t{img.name}\t{img.size}\t{img.mode}')
 
-def scan_image(image, width, height):
+def scan_image(image, width:int, height:int) -> list:
     image_width, image_height = image.size
-    pixel_width = image_width / width
-    pixel_height = image_height / height
-
+    x_segments = n_exclusive_segments(image_width, width)
+    y_segments = n_exclusive_segments(image_height, height)
     segment_list = []
 
-    for h_index in range(height):
-        for w_index in range(width):
-            start_width = (w_index * pixel_width)
-            start_height = (h_index * pixel_height)
-            segment_list.append((round(start_width), 
-                                 round(start_width+pixel_width), 
-                                 round(start_height), 
-                                 round(start_height+pixel_height)))
-            # no overlapping pixels
-            temp = list(segment_list.pop())
-            if(h_index != height -1 and w_index != width - 1):
-                temp[1] = temp[1] - 1
-                temp[3] = temp[3] - 1
-            elif(h_index != height - 1):
-                temp[3] = temp[3] - 1
-            elif(w_index != width - 1):
-                temp[1] = temp[1] - 1
-            segment_list.append(tuple(temp))
+    for y_seg in y_segments:
+        for x_seg in x_segments:
+            segment_list.append((x_seg[0], x_seg[1], y_seg[0], y_seg[1]))
+
     return segment_list
 
-def select_image(imglist):
+#def scan_image(image, width:int, height:int) -> list:
+#    image_width, image_height = image.size
+#    pixel_width = image_width / width
+#    pixel_height = image_height / height
+#
+#    segment_list = []
+#
+#    for h_index in range(height):
+#        for w_index in range(width):
+#            start_width = (w_index * pixel_width)
+#            start_height = (h_index * pixel_height)
+#            segment_list.append((round(start_width), 
+#                                 round(start_width+pixel_width), 
+#                                 round(start_height), 
+#                                 round(start_height+pixel_height)))
+#            # no overlapping pixels
+#            temp = list(segment_list.pop())
+#            if(h_index != height -1 and w_index != width - 1):
+#                temp[1] = temp[1] - 1
+#                temp[3] = temp[3] - 1
+#            elif(h_index != height - 1):
+#                temp[3] = temp[3] - 1
+#            elif(w_index != width - 1):
+#                temp[1] = temp[1] - 1
+#            segment_list.append(tuple(temp))
+#    return segment_list
+
+def select_image(imglist:list)->int:
     prompt = 'Please select an image to output in ascii: ' 
     print_imglist(imglist)
 
@@ -118,12 +208,27 @@ def select_image(imglist):
         imgnum = tryparse_int(input(prompt))
     return imgnum
 
-def sub_image_to_key(sub_image, dict_len):
-    delimiter = 255 / (dict_len - 1)
-    avg = np.average(sub_image)
-    return floor(avg/delimiter) 
+def sub_image_to_key(sub_image, dict_len:int, image)->int:
+    default_transform = lambda sub_image, image: np.mean(sub_image)
+    default_delimiter = lambda num, dict_len: floor(num / (COLOR_MAX / (dict_len - 1)))
 
-def tryparse_int(str):
+    match TRANSFORM:
+        case 0: 
+            transform = default_transform
+        case 1:
+            transform = lambda sub_image, image: np.median(sub_image)
+        case _:
+            transform = default_transform
+
+    match DELIMITER:
+        case 0:
+            delimit = default_delimiter
+        case _:
+            delimit = default_delimiter
+
+    return decide_classification(sub_image, image, dict_len, transform, delimit)
+
+def tryparse_int(str : str) -> int:
     val = -1
     try:
         val = int(str)
@@ -132,7 +237,7 @@ def tryparse_int(str):
         quit()
     return val
 
-def validate_list(segment_list, width, height):
+def validate_list(segment_list:list, width:int, height:int) -> list:
     for i in range(0, len(segment_list)):
         temp = list(segment_list[i])
         if(temp[0] > width):
@@ -145,6 +250,9 @@ def validate_list(segment_list, width, height):
             temp[3] = height
         segment_list[i] = tuple(temp)
     return segment_list
+
+def validate_size(axis_size:int, num_segments:int)->bool:
+    return not (num_segments) > (axis_size + 1)/2
 
 if __name__ == '__main__':
     main()
