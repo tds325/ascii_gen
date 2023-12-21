0fa3834bf9a86ca3637764738990f7e6df8aa473
tds325 <tds325@nau.edu>
2023-12-20 17:10:42 -0700

    cdf stretching, delimit from bounds strategy, simplifying delimit selection strategy

--- ascii.py
+++ ascii.py
@@ -1,48 +1,38 @@
 from __future__ import annotations
 import argparse
 import typing
-import time
-from functools import reduce
 import numpy as np
 import os
-from math import floor, ceil, sqrt
+import sys
+from math import floor, sqrt
 from PIL import Image
 from textwrap import dedent
 
-ASPECT_LEEWAY = 0.9
+ASPECT_LEEWAY=0.9
 COLOR_MAX=255
 DELIMITER=0
-H_MULTIPLIER = 3
-MAX_OUTPUT_HEIGHT = 50
-MAX_OUTPUT_WIDTH = 160
+H_MULTIPLIER=3
+HIST_MIN=0.015
+MAX_OUTPUT_HEIGHT=50
+MAX_OUTPUT_WIDTH=160
 PRE_COMPUTE=0
 TRANSFORM=1
 
-SMALL_DEFAULT = 20
-MED_DEFAULT = 36
-LRG_DEFAULT = 65
-XLRG_DEFAULT = 88
+SMALL_DEFAULT=20
+MED_DEFAULT=36
+LRG_DEFAULT=65
+XLRG_DEFAULT=88
 
 class ImageData:
     def __init__(self, image, ascii_dict_len):
         self.image = image
         self.ascii_dict_len = ascii_dict_len
         self.pre_compute = {}
+        self.transform:typing.Any = None
+        self.delimit:typing.Any = None
 
     def set_sub_image(self, sub_image:np.ndarray)->ImageData:
         self.sub_image = sub_image
-        return self
-
-    def set_image(self, image:np.ndarray)->ImageData:
-        self.image = image
-        return self
-
-    def set_ascii_dict_len(self, ascii_dict_len:int)->ImageData:
-        self.ascii_dict_len = ascii_dict_len
-        return self
-
-    def set_pre_compute(self, pre_compute:dict[str,typing.Any])->ImageData:
-        self.pre_compute = pre_compute
         return self
 
     def set_pre_compute_pair(self, kv_pair:tuple[str,typing.Any])->dict[str,object]:
@@ -81,11 +71,9 @@ def adjust_to_aspect_ratio(aspect_ratio:float, width:int, height:int)->tuple[int
             width -= 1
     return (width,height)
     
-def decide_classification(image_info:ImageData, 
-                          data_transform: typing.Callable[[ImageData], float], 
-                          delimiter: typing.Callable[[float,ImageData],int]) -> int:
-    num = data_transform(image_info)
-    ascii_class = delimiter(num, image_info)
+def decide_classification(image_info:ImageData)->int:
+    num = image_info.transform(image_info)
+    ascii_class = image_info.delimit(num, image_info)
     return ascii_class
 
 def determine_image_size(image:Image.Image, size:str)->tuple[int, int]:
@@ -128,46 +116,64 @@ def determine_segment_values(image_data:ImageData,
         segment_key_list.append(sub_image_to_key(image_data))
     return segment_key_list
 
+def find_bounds(np_arr):
+    length = len(np_arr)
+    bounds = [0, length-1]
+    index = 0
+    while index < length // 2:
+        if(np_arr[index] < HIST_MIN):
+            bounds[0] += 1
+        if(1.0 - np_arr[length-1-index] < HIST_MIN):
+            bounds[1] -= 1
+        index += 1
+    return bounds
+
 def gamma_expansion(np_arr:np.ndarray)->np.ndarray:
     np_arr = np.where(np_arr <= 0.04045, np_arr / 12.92, ((np_arr+0.055)/1.055)**2.4)
     return np_arr
 
-def histogram_from_arr(np_arr:np.ndarray, size:int)->np.ndarray:
-    histogram = np.asarray([0 for _ in range(size)])
-    if(len(np_arr.shape)==3):
-        temp_arr = np_arr[:,:,0]
-        row, col, dim = np_arr.shape
+def get_config_file(args:argparse.Namespace):
+    config_file = []
+    def tryfile(filename):
+        try:
+            return open(filename,"r")
+        except:
+            print("Unable to open config file, exiting program")
+    if(args.config != None):
+        config_file = tryfile(args.config)
     else:
-        temp_arr = np_arr
-        row, col = np_arr.shape
-    for r in range(row):
-        for c in range(col):
-            temp_arr[r,c] = np.mean(np_arr[r,c])
-            histogram[tryparse_int(temp_arr[r,c]*size-1)] += 1
-    histogram = histogram / (row*col)
-    return histogram
+        file_list = os.listdir(os.getcwd())
+        for file in file_list:
+            if file.endswith(".cfg"):
+                config_file = tryfile(file)
+                break
+    return config_file
 
 def histogram_normalization(np_arr:np.ndarray)->np.ndarray:
-    histogram = histogram_from_arr(np_arr, COLOR_MAX+1)
+    rgbhist_list = [np.histogram(np_arr[:,:,i]*COLOR_MAX,bins=range(COLOR_MAX+2), density = True)[0] for i in range(3)]
 
-    # find first and last nonzero
-    nonzero = [i for i,bool in enumerate(np.where(histogram != 0, True, False)) if bool]
-    if(len(nonzero) > 0):
-        first, last = nonzero[0], nonzero[-1]
+    rgb_cdf = []
+    for i in range(3):
+        cdf = rgbhist_list[i].cumsum()
 
-        nonzero = nonzero[first:last+1]
+        # transform cdf 
+        # (translate first val to (0,0) then scale so last value = (256,1.0)
+        bounds = find_bounds(cdf)
+        cdf = cdf[bounds[0]:bounds[1]+1]
+        indices = np.arange(0, len(cdf))
+        cdf_enum = np.column_stack((indices,cdf))
+        cdf_enum[:,1] -= cdf_enum[0,1]
+        x_quot = float(COLOR_MAX+1) / cdf_enum[-1][0]
+        y_quot = float(1.0) / cdf_enum[-1][1]
+        scale_matrix = np.array([[x_quot,0],[0,y_quot]])
+        stretched = cdf_enum.dot(scale_matrix).T
 
-        cdf = [reduce(lambda x,y: x+y, histogram[:i]) for i in range(1,COLOR_MAX+2)]
-        #cdf = [np.ceil(x * COLOR_MAX) for x in cdf]
+        interpolated = np.interp(range(COLOR_MAX+1), stretched[0], stretched[1])
+        rgb_cdf.append(interpolated)
 
-        # histogram normalization algorithm
-        stretch = lambda x: np.asarray([cdf[round(y*COLOR_MAX)] for y in x])
-
-        row,col = np_arr.shape[:2]
-        for r in range(row):
-            for c in range(col):
-                np_arr[r,c,:] = stretch(np_arr[r,c,:])
-
+    rgb_list = np.split(np_arr, 3, axis=2)
+    rgb_list = [np.interp(rgb_list[i]*COLOR_MAX, range(COLOR_MAX+1), rgb_cdf[i]) for i in range(3)]
+    np_arr = np.concatenate(rgb_list, axis = 2)
     return np_arr
 
 def initialize_arg_parser()->argparse.ArgumentParser:
@@ -175,33 +181,32 @@ def initialize_arg_parser()->argparse.ArgumentParser:
     parser.add_argument('-n', '--n-chars', 
                         help="Number of possible characters to be derived from the image",
                         default=71,
-                        required = False)
+                        required=False)
     parser.add_argument('-f', '--file', 
                         help="Choose an image file directly instead of through the program", 
-                        required = False)
+                        required=False)
     parser.add_argument('-d', '--directory', 
                         help="Choose an image directory to select a possible image from",
                         default='images/',
-                        required = False)
+                        required=False)
     parser.add_argument('-s', '--size', 
                         help="image size selection from [small, medium, large, xlarge]",
-                        default = "large",
-                        required = False)
+                        default="large",
+                        required=False)
     parser.add_argument('-o', '--output',
                         help="output to a file instead of stdio",
-                        required = False)
+                        required=False)
     parser.add_argument('-dl', '--delimiter',
                         help="define how image classifications are spread among possible values",
-                        default = 1,
-                        required = False)
+                        default=1,
+                        required=False)
     parser.add_argument('-bg', '--background',
                         help="output intended for light or dark background",
-                        default = "dark",
-                        required = False)
-    #parser.add_argument('-r', '--range',
-    #                    help="select a range more narrow (<255) in cases of images where the values are too similar in regular use",
-    #                    required = False)
-
+                        default="dark",
+                        required=False)
+    parser.add_argument('-c', '--config',
+                        help="specify the location of a config file for global variables",
+                        required=False)
     return parser
 
 def initialize_ascii_dict(size:int)->dict[int,str]:
@@ -263,9 +268,18 @@ def initialize_ascii_dict(size:int)->dict[int,str]:
     return ascii_dict
 
 def initialize_globals(args:argparse.Namespace):
+    thismodule = sys.modules[__name__]
+    # command line arguments will override any config file settings
+    config_file = get_config_file(args)
+    if(config_file != None):
+        for line in config_file:
+            var, val = line.strip().split("=")
+            if var in ["ASPECT_LEEWAY"]:
+                setattr(thismodule, var, float(val))
+            else:
+                setattr(thismodule, var, int(val))
     if(args.delimiter != None):
-        global DELIMITER
-        DELIMITER = tryparse_int(args.delimiter)
+        setattr(thismodule, "DELIMITER", tryparse_int(args.delimiter))
 
 def input_images(files:list, dir:str)->list[Image.Image]:
     imglist = []
@@ -335,9 +349,6 @@ def output_to_ascii(image:Image.Image, args:argparse.Namespace):
     pre_compute(image_data)
     segment_key_list = determine_segment_values(image_data, segment_list)
 
-    # Palette mode images store color values differently
-    std_dev = np.mean(image_data.get_pre_compute_by_key("std-dev"))
-
     flip_key = lambda key, dict_len: (-key + (dict_len-1)) if args.background == "dark" else key
 
     output = ""
@@ -362,57 +373,73 @@ def output_to_ascii(image:Image.Image, args:argparse.Namespace):
     else:
         print(output)
 
-
 def pre_compute(image_data:ImageData)->ImageData:
     match(PRE_COMPUTE):
         case _:
             std_dev = [np.std(image_data.image[:,:,i].flatten()) for i in range(3)]
     image_data.set_pre_compute_pair(("std-dev",std_dev))
 
-    total_mean = np.mean(image_data.image)
-
-    distance_to_ends = (COLOR_MAX - total_mean, total_mean)
-
-    max_n_root = [sqrt(dist) for dist in distance_to_ends]
-    # allow for left and right ends to have some space
-    # todo more accurate spacing
-    max_n_root[0] *= 0.9
-    max_n_root[1] *= 0.9
-
-    # greater side gets more delimiting boundaries
-    total_boundaries = image_data.ascii_dict_len - 1
-    if(distance_to_ends[0] > distance_to_ends[1]):
-        rhs = total_boundaries // 2
-        lhs = total_boundaries - rhs
-    else:
-        lhs = total_boundaries // 2
-        rhs = total_boundaries - lhs
-
-    left_quotient = max_n_root[0] / lhs
-    right_quotient = max_n_root[1] / rhs
-
     delimit_list = []
-    for i in range(lhs,0,-1):
-        delimit_list.append(total_mean + (left_quotient*i)**2)
-    for i in range(1,rhs+1):
-            delimit_list.append(total_mean - (right_quotient*i)**2)
+    match(DELIMITER):
+        case 1:
+            total_mean = np.mean(image_data.image)
+            distance_to_ends = (COLOR_MAX - total_mean, total_mean)
+            max_n_root = [sqrt(dist) for dist in distance_to_ends]
+            # allow for left and right ends to have some space
+            max_n_root[0] *= 0.99
+            max_n_root[1] *= 0.99
+            # greater side gets more delimiting boundaries
+            total_boundaries = image_data.ascii_dict_len - 1
+            if(distance_to_ends[0] > distance_to_ends[1]):
+                rhs = total_boundaries // 2
+                lhs = total_boundaries - rhs
+            else:
+                lhs = total_boundaries // 2
+                rhs = total_boundaries - lhs
 
+            left_quotient = max_n_root[0] / lhs
+            right_quotient = max_n_root[1] / rhs
+
+            for i in range(lhs,0,-1):
+                delimit_list.append(total_mean + (left_quotient*i)**2)
+            for i in range(1,rhs+1):
+                delimit_list.append(total_mean - (right_quotient*i)**2)
+        case 2:
+            total_boundaries = image_data.ascii_dict_len + 1
+            max_x = sqrt(COLOR_MAX)
+            increment = float(max_x) / total_boundaries
+            inc_list = [(x*increment)**2 for x in range(total_boundaries)]
+            inc_list = inc_list[1:-1]
+
+            for x in range(len(inc_list)):
+                if x % 2 == 0:
+                    delimit_list.append(inc_list[x])
+                else:
+                    delimit_list.append(COLOR_MAX - inc_list[x])
+            delimit_list.sort()
+        case _:
+            total_boundaries = image_data.ascii_dict_len
+            increment = float(COLOR_MAX) / total_boundaries
+            for x in range(1,total_boundaries):
+                delimit_list.append(x*increment)
     image_data.set_pre_compute_pair(("delimit_list",delimit_list))
 
     return image_data
 
 def preprocess_image(image:np.ndarray)->np.ndarray:
-    start = time.time()
-    
     temp_arr = image / COLOR_MAX
     
     temp_arr = gamma_expansion(temp_arr)
-    #linear_approx = lambda rgb: [0.299*rgb[0], 0.587*rgb[1], 0.144*rgb[2]]
+
+    #linear approx instead of gamma expansion:
+    #temp_arr[:,:,0]= temp_arr[:,:,0]*0.299
+    #temp_arr[:,:,1]= temp_arr[:,:,1]*0.587
+    #temp_arr[:,:,2]= temp_arr[:,:,2]*0.144
 
     # scalar value between 0 and 1 for interpolation of normalized image 
     # to increase clarity of image, set above 1.0 (some information will be lost)
     # TODO: how to decide best scalar? *add default strategy and cmd line option
-    scalar = 0.0
+    scalar = 1.0
     hist_arr = histogram_normalization(temp_arr.copy())
     temp_arr = (temp_arr*(1.0-scalar) + hist_arr*scalar)
     temp_arr = np.clip(temp_arr,0.0,1.0)
@@ -420,7 +447,6 @@ def preprocess_image(image:np.ndarray)->np.ndarray:
     # TODO: add dithering to compensate for quantization error
 
     image = temp_arr * COLOR_MAX
-    print(time.time() - start)
     return image
 
 def segment_image(image:Image.Image, width:int, height:int) -> list[tuple[int,int,int,int]]:
@@ -447,35 +473,27 @@ def select_image(imglist:list[Image.Image])->int:
         imgnum = tryparse_int(input(prompt))
     return imgnum
 
+def set_global(name:str,value:typing.Any):
+    exec("global "+name)
+    exec(name + f"={value}")
+
 def sub_image_to_key(image_data:ImageData)->int:
-    default_transform = lambda image_data: np.mean(image_data.sub_image)
-    default_delimiter = lambda num, image_data: floor(num / (COLOR_MAX / (image_data.ascii_dict_len - 1)))
-
-    match TRANSFORM:
-        case 0: 
-            transform = default_transform
-        case 1:
-            transform = lambda image_data: np.median(image_data.sub_image)
-        case _:
-            transform = default_transform
-
-    match DELIMITER:
-        case 0:
-            delimit = default_delimiter
-        case 1: 
-            # group delimiting numbers around mean
-            def squared_dist_from_mean_delimit(num, image_data):
-                ascii_key = image_data.ascii_dict_len - 1
-                for delimiter in image_data.get_pre_compute_by_key("delimit_list"):
-                    if(num < delimiter):
-                        ascii_key -= 1
-                
-                return ascii_key
-            delimit=squared_dist_from_mean_delimit
-        case _:
-            delimit = default_delimiter
-
-    return decide_classification(image_data, transform, delimit)
+    if(image_data.transform == None):
+        default_transform = lambda image_data: np.mean(image_data.sub_image)
+        match TRANSFORM:
+            case 1:
+                image_data.transform = lambda image_data: np.median(image_data.sub_image)
+            case _:
+                image_data.transform = default_transform
+    if(image_data.delimit == None):
+        def delimit_from_list(num, image_data):
+            ascii_key = image_data.ascii_dict_len - 1
+            for delimiter in image_data.get_pre_compute_by_key("delimit_list"):
+                if(num < delimiter):
+                    ascii_key -= 1
+            return ascii_key
+        image_data.delimit=delimit_from_list
+    return decide_classification(image_data)
 
 def tryparse_int(str : str) -> int:
     val = -1
